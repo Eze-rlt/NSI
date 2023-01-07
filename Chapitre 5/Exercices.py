170cc7d15e314f6b9e4e7eacf8c0eb05f6d9ed40
rasp.eze@yahoo.com <ezechiel.renault@gmail.com>
2023-01-07 17:46:01 +0100

    07/01/23

--- Chapitre 5/Exercices.py
+++ Chapitre 5/Exercices.py
@@ -1,43 +1,18 @@
 def ex1() -> str:
     return "5x1000 + 6x100 + 2x10 + 4x1\n5x10³ + 6x10² + 2x10¹ + 4x10⁰"
 
-def decimal_to_binaire(nbe: int) -> int:
-    assert type(nbe) == int and nbe>=0, "Le nombre doit être un entier supérieur ou égal à 0"
-
+def base(nbe: int, base_src: int = 10, base_dest: int = 2) -> int:
+    assert type(nbe) == int and nbe>=0, "Le nombre à convertir doit être un entier supérieur ou égal à 0"
+    assert type(base_src) == int and base_src>0, "La source de base doit être un entier supérieur à 0"
+    assert type(base_dest) == int and base_dest>0, "La destination de base doit être un entier supérieur à 0"
     p = q = 0
     while nbe>0:
-        p+=(nbe%2)*10**q
+        p+=(nbe%base_dest)*base_src**q
         q+=1
-        nbe//=2
+        nbe//=base_dest
     
     return p
 
-def binaire_to_decimal(nbe: int) -> int:
-    assert type(nbe) == int and nbe>=0, "Le nombre doit être un entier supérieur ou égal à 0"
-    nbe = str(nbe)
-    n2 = int()
-    p = 0
-    for n in nbe[::-1]:
-        n2+=int(n)*2**p
-        p+=1
-    return n2
 
-def binaire_to_decimal2(nbe: int) -> int:
-    assert type(nbe) == int and nbe>=0, "Le nombre doit être un entier supérieur ou égal à 0"
-    
-    nbe = [int(k) for k in str(nbe)][::-1]
-    n = 0
-    p = 1
-    for x in range(len(nbe)):
-        n += nbe[x]*p
-        p*=2
-    return n
-
-    
-
-
-nbe = 58
-b = decimal_to_binaire(nbe)
-d = binaire_to_decimal2(b)
-
-print(b, d)
+nbe = 39
+print(base(nbe, 2, 10))
