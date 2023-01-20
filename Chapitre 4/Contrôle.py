dc76c75c652a2baf35ab3d3e0f64dfeac6ba2335
rasp.eze@yahoo.com <ezechiel.renault@gmail.com>
2023-01-20 14:37:50 +0100

    20/12/23

--- Chapitre 4/Contrôle.py
+++ Chapitre 4/Contrôle.py
@@ -1,36 +1,28 @@
-def occurrence_dans_liste(lst: list, n: int) -> int:
-    "Retourne le nombre d'occurrence d'un entier dans une liste"
+def occurrence_dans_liste(lst, n):
+    nombre_de_fois = 0
+    for nombre in lst:
+        if nombre == n:
+            nombre_de_fois = nombre_de_fois+1
     
-    nbe_fois = 0
-    for nbe in lst:
-        if nbe == n:
-            nbe_fois+=1
-    
-    return nbe_fois
+    return nombre_de_fois
 
-def cout_puits(n: int) -> int:
-    "retourne le prix pour creuser un trou de n mètres de profondeur"
-
-    assert n > 0, 'Le nombre de mètres doit être supérieur à 0'
-    prix = 800
+def cout_puits(n):
+    prix_total = 800
+    prix_mètre=50
     for x in range(n):
-        prix+=50+(5*x)#CALCULE LE PRIX DU METRE ET L'AJOUTE AU PRIX FINAL
-    return prix
+        prix_total = prix_total+prix_mètre
+        prix_mètre = prix_mètre+5
 
-def attendre(objectif: float) -> int:
-    argent = 1200
-    assert type(objectif) == int and objectif>argent, "L'objectif doit être un entier supérieur au capital de base."
-    nbe_années = 0
+    return prix_total
 
-    while argent < objectif:
-        nbe_années+=1
-        argent+=(1.3/100)*argent
+def attendre(objectif):
+    money = 1200
+    assert objectif>money, "L'objectif doit être un entier supérieur au capital de base."
+    nombre_années = 0
+
+    while money < objectif:
+        nombre_années = nombre_années+1
+        money = money+(1.3/100)*money
     
-    assert nbe_années < 100, "Le nombre d'années est trop grand, vous serez déjà mort !!"
-    return nbe_années
-
-"""
-occurrence_dans_liste([1, 2, 3, 4, 5], 3) retourne 1
-occurrence_dans_liste([1, 2, 3, 4, 5, 3], 3) retourne 2
-occurrence_dans_liste([1, 2, 4, 5], 3) retourne 0
-"""
+    assert nombre_années <= 100, "Le nombre d'années est plus grand que 0"
+    return nombre_années
