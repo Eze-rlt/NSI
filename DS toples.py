3f0152711daef70cfee8c7bab68d5f80269ba542
rasp.eze@yahoo.com <ezechiel.renault@gmail.com>
2023-05-12 14:25:03 +0200

    12/05/2023

---
+++ DS toples.py
@@ -0,0 +1,34 @@
+import random
+
+def tableau_aleatoire(n:int, a:int, b:int) -> list:
+    tab = list()
+    for x in range(n):
+        aléatoire = random.randint(a, b)
+        tab.append(aléatoire)
+    return tab
+
+def melange(tab:list) -> list:
+    taille_tab = len(tab)
+    for i in range(taille_tab):
+        j = random.randint(0, i)
+        valeur1 = tab[i]
+        valeur2 = tab[j]
+        tab[i] = valeur2
+        tab[j] = valeur1
+    return tab
+
+def est_reçu(liste_prenoms:tuple, utilisateur:str) -> bool:
+    reçu = False
+    for n in liste_prenoms:
+        if utilisateur==n:
+            reçu = True
+    return reçu
+
+def trouver(lst:list, val:int)->list:
+    places = list()
+    taille_liste = len(lst)
+
+    for x in range(0, taille_liste):
+        if lst[x]==val:
+            places.append(x)
+    return places
