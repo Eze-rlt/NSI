293af744bcf69bd2d381c70cf5e345dc330b770c
rasp.eze@yahoo.com <ezechiel.renault@gmail.com>
2023-05-12 19:04:39 +0200

    fixing som bugs to detect the victory

--- Mini-games/Puissance 4.py
+++ Mini-games/Puissance 4.py
@@ -1,17 +1,23 @@
 from os import system
+from random import randint
 
 class colors:
+    system('')
     yellow = '\33[93m'
     red = '\33[91m'
     grey = '\33[90m'
     no = '\33[0m'
+def reset_grille():
+    global grille
+    grille = [[None for s in range(6)] for x in range(7)]
 
-grille = [[None for s in range(6)] for x in range(7)]
+grille = list
+reset_grille()
 
 def clear():
     system('cls')
 
-def place_grille(x: int, joueur: int):
+def place_jeton(x: int, joueur: int):
     jeton = '0' if joueur == 1 else 'X'
     x-=1
 
@@ -45,52 +51,103 @@ def check_fini():
                 if ligne[x] == ligne[x+1] == ligne[x+2] == ligne[x+3] != None:
                     return ligne[x]
     def check_ligne():
-        for x in range(3):
+        for x in range(4):
             for y in range(6):
                 if grille[x][y] == grille[x+1][y] == grille[x+2][y] == grille[x+3][y] != None:
                     return grille[x][y]
-    def check_diago1():#BAS-> HAUT ; GAUCHE -> DROITE
-        for x in range(4):
-            for y in range(3):
-                if grille[x][y] == grille[x+1][y+1] == grille[x+2][y+2] == grille[x+3][y+3] != None:
-                    #LES DIAGOS -> VERS HAUT ; GAUCHE -> DROITE
-                    print(1)
+
+    def check_diago1():
+        for y in range(-3, 0):
+            for x in range(4):
+                if (grille[x][y]==grille[x+1][y-1]==grille[x+2][y-2]==grille[x+3][y-3]!=None):
                     return grille[x][y]
-    def check_diago2():#BAS-> HAUT ; DROITE -> GAUCHE
-        for x in range(3, 6):
-            for y in range(3):
-                if grille[x][y] == grille[x-1][y+1] == grille[x-2][y+2] == grille[x-3][y+3] != None:
-                    #LES DIAGOS BAS VERS HAUT? GAUCHE VERS DROITE
-                    print(2)
+    
+    def check_diago2():
+        for y in range(-3, 0):
+            for x in range(3, 7):
+                if (grille[x][y]==grille[x-1][y-1]==grille[x-2][y-2]==grille[x-3][y-3]!=None):
                     return grille[x][y]
+
+    
     
     r=None
-    r = check_ligne()
-    r = check_columns() if not r else r
-    r = check_diago1() if not r else r
-    r = check_diago2() if not r else r
-    if r: return r
-
-end=None
-clear()
-while not end:
-    print_grille()
-    j1 = int(input('Joueur 1, à toi !! '))
-    while grille[j1-1][0] != None or not (1<=j1<=7):
-        j1 = int(input('La columne est déjà pleine, réessaie !! '))
-    place_grille(j1, 1)
-    end=check_fini()
-    clear()
-
+    r = check_columns()
+    if not r: r = check_ligne()
+    if not r: r = check_diago1()
+    if not r: r = check_diago2()
     
-    if not end:
-        print_grille()
-        j2 = int(input('Joueur 2, à toi !! '))
-        while grille[j2-1][0] != None or not (1<=j2<=7):
-            j2 = int(input('La columne est déjà pleine, réessaie !! '))
-        place_grille(j2, 2)
-        end=check_fini()
-        clear()
+    return r
 
-print_grille()
-print('Le joueur', end, 'a gagné !!')
+def play(joueur:int) -> int:
+    clear()
+    print_grille()
+    coup = int(input(f'Joueur {joueur}, à toi !! '))
+    while grille[coup-1][0] != None or not (1<=coup<=7):
+        coup = int(input('La columne est déjà pleine, réessaie !! '))
+    place_jeton(coup, joueur)
+
+def manual_play():
+    end=None
+    while not end:
+        play(1)
+        end=check_fini()
+        if end:break
+
+        play(2)
+        end=check_fini()
+
+        if not None in [grille[x][0] for x in range(grille)]:
+            end=True
+
+
+    print_grille()
+    if end!=True:
+        print('Le joueur', end, 'a gagné !!')
+    else:
+        print('Ex-Eaco !!!')
+
+def debug_mode_player(coups1:list, coups2:list=[]):
+    clear()
+    for coup in coups1:
+        place_jeton(coup, 1)
+    for coup in coups2:
+        place_jeton(coup, 2)
+    print_grille()
+    print('Le joueur', check_fini(), 'a gagné !! (DEBUG MODE)')
+    
+exit()
+def autoplay():
+    def auto_play(joueur:int) -> int:
+        print_grille()
+        coup = randint(1, 7)
+        while grille[coup-1][0] != None or not (1<=coup<=7):
+            coup = randint(1, 7)
+        place_jeton(coup, joueur)
+        print()
+    
+    end=None
+    while not end:
+        auto_play(1)
+        end=check_fini()
+        if end:break
+
+        auto_play(2)
+        end=check_fini()
+
+        if not None in [grille[x][0] for x in range(len(grille))]:
+            end=True
+
+
+    print_grille()
+    if end!=True:
+        print('Le joueur', end, 'a gagné !! (DEBUG MODE)')
+    else:
+        print('Ex-Eaco !!! (DEBUG MODE)')
+        for a in grille:
+            print(a)
+
+autoplay()
+while not input('CONTINUE ? ').lower() == 'n':
+    reset_grille()
+    autoplay()
+
